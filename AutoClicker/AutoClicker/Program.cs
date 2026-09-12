c68057346ef8079b17085b4ee2ea124df2f28cd5
Kieran Huey <126865879+DaBoiKreexz@users.noreply.github.com>
2026-09-12 12:25:36 -0500

    Add files via upload

---
+++ AutoClicker/AutoClicker/Program.cs
@@ -0,0 +1,55 @@
+﻿using AutoClicker;
+using System.Runtime.InteropServices;
+
+Renderer renderer = new Renderer();
+renderer.Start().Wait();
+
+[DllImport("user32.dll")]
+static extern short GetAsyncKeyState(int vKey);
+
+// HotKeys
+const int enableClicker = 0x75;
+const int disableClicker = 0x76;
+
+while (true) 
+{
+    if ((GetAsyncKeyState(enableClicker) & 0x8000) != 0)
+    {
+        renderer.enableAutoClicker = true;
+    }
+    else if ((GetAsyncKeyState(disableClicker) & 0x8000) != 0)
+    {
+        renderer.enableAutoClicker = false;
+    }
+
+    if (renderer.enableAutoClicker)
+    {
+        // Left Click
+        if(renderer._selectedIndex == 0) 
+        {
+            mouse_event(MouseConstants.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep(10);
+            mouse_event(MouseConstants.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Thread.Sleep(20);
+        }
+        // Right Click
+        else if(renderer._selectedIndex == 1)
+        {
+            mouse_event(MouseConstants.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+            Thread.Sleep(10);
+            mouse_event(MouseConstants.MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+            Thread.Sleep(20);
+        }
+    }
+}
+
+[DllImport("user32.dll")]
+static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+static class MouseConstants
+{
+    public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+    public const int MOUSEEVENTF_LEFTUP = 0x04;
+    public const int MOUSEEVENTF_RIGHTDOWN = 0x0008; 
+    public const int MOUSEEVENTF_RIGHTUP = 0x0010; 
+}
---
+++ AutoClicker/AutoClicker/Renderer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+
+
+namespace AutoClicker
+{
+    internal class Renderer : Overlay
+    {
+
+        public bool enableAutoClicker;
+        public string[] _items = { "Mouse 1", "Mouse 2" };
+        public int _selectedIndex = 0;
+        
+        protected override void Render()
+        {
+            ImGui.Begin("Auto Clicker (By Kreexz)");
+
+            string currentPreviewValue = _items[_selectedIndex];
+            if (ImGui.BeginCombo("Select Options", currentPreviewValue))
+            {
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    bool isSelected = (_selectedIndex == i);
+
+                    if (ImGui.Selectable(_items[i], isSelected))
+                    {
+                        _selectedIndex = i; 
+                    }
+
+                    if (isSelected)
+                    {
+                        ImGui.SetItemDefaultFocus();
+                    }
+                }
+                ImGui.EndCombo();
+            }
+
+            ImGui.Checkbox("Auto Clicker", ref enableAutoClicker);
+
+            ImGui.End();
+        }
+    }
+}
